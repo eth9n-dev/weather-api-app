9cb3d2b47d4e437b003c598e653d818d98204854
eth9n-dev <eckert6@gmail.com>
2024-01-02 19:39:32 -0800

    weather condition animations
    added rain + snow, extra error handling

--- app.py
+++ app.py
@@ -12,25 +12,36 @@ def main_page():
     URL = 'http://api.weatherapi.com/v1/current.json'
     
     if request.method == 'POST':
-        if request.form['zipcode']:
+        if request.form['zipcode']: # Ensure we receive a valid location to send to the API
             location = request.form['zipcode']
             PARAMS = {'key': key, 'q': location, 'aqi': 'no'}
             r = requests.get(url = URL, params = PARAMS)
             data = r.json()
 
-            if r.status_code == 200:
+            if r.status_code == 200: # Valid response
                 city = data['location']['name']
                 state = data['location']['region']
                 local_time = data['location']['localtime']
                 temperature_f = data['current']['temp_f']
                 temperature_c = data['current']['temp_c']
                 condition = data['current']['condition']['text']
-
-                return render_template("weather.html", city=city, state=state, local_time=local_time, temperature_f=temperature_f, temperature_c=temperature_c,
+                
+                # weather condition checks
+                if 'snow' in condition:
+                    return render_template("snow.html", city=city, state=state, local_time=local_time, temperature_f=temperature_f, temperature_c=temperature_c,
                                        condition=condition) 
+                
+                if 'rain' in condition:
+                    return render_template("rain.html", city=city, state=state, local_time=local_time, temperature_f=temperature_f, temperature_c=temperature_c,
+                                       condition=condition)
+                
+                else:
+                    return render_template("fallback.html", city=city, state=state, local_time=local_time, temperature_f=temperature_f, temperature_c=temperature_c,
+                                       condition=condition)
 
             else:
                 print('Error, Status Code: ' + str(r.status_code))
+                return render_template("home.html", error=True)
 
     return render_template("home.html")
 
